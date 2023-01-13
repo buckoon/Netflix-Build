7d3d88a18214bb051bde5cfeb59f2fd999bfb4c1
buckoon <jeremy8@vt.edu>
2023-01-13 16:48:30 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import HomeScreen from "./HomeScreen";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from "./screens/Login"
 
 
 
 import './App.css';
 
 function App() {
+
+  const user = null;
   return (
     <div className="app">
       <Router>
+        {!user ? (
+          <Login/>) : (
+       
         <Routes>
           <Route  path="/" element={<HomeScreen />}/>
         </Routes>
+          )}
       </Router>
     </div>
   );
---
+++ src/screens/Login.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./Login.css"
+
+function Login() {
+  return (
+    <div className="login">
+      <div className="loginScreen_background">
+        <img
+        className="loginscreen_logo"
+         src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt=""/>
+
+
+      </div>
+      <button className="loginScreen_button">
+       
+          Sign In
+      </button>
+      <div className="loginScreen_gradient">
+
+        
+      </div>
+        
+        
+    </div>
+  )
+}
+
+export default Login
