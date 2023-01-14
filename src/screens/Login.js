0e55720afec8c0377434704a22b61a44eb96bdfd
buckoon <jeremy8@vt.edu>
2023-01-14 15:50:52 -0500

    WIP

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,22 +1,69 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Login.css"
+import SignInScreen from './SignInScreen';
 
 function Login() {
+
+  const[ signin, setSignin ]=useState(false);
+
   return (
     <div className="login">
-      <div className="loginScreen_background">
+      <div className="loginscreen_background">
         <img
         className="loginscreen_logo"
          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt=""/>
 
 
       </div>
-      <button className="loginScreen_button">
+
+
+      <button onClick={() => setSignin(true)}  
+      
+      className="loginscreen_button">
        
           Sign In
       </button>
-      <div className="loginScreen_gradient">
+      <div className="loginscreen_gradient">
 
+
+      </div>
+
+      <div className="loginscreen_body">
+        {signin ? (
+          <SignInScreen/>
+
+        ):( 
+        
+          <>
+        <h1>
+          Unlimited films
+        </h1>
+
+        <h2>Watch anywhere and cancel at anytime</h2>
+
+        <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+
+        <div className= "loginscreen_input">
+          <form>
+            <input type="email" placeholder="email"
+            />
+            <button 
+            onClick={() => setSignin(true)}
+            className = "getstarted_button">Get Started
+
+
+            </button>
+
+
+
+           
+
+          </form>
+
+        </div>
+        </>
+        )}
+       
 
       </div>
      
---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./SignInScreen.css"
+
+function SignInScreen() {
+  return (
+    <div className="SignInScreen">
+        <form>
+            <h1>Sign In</h1>
+            <input placeholder="Email" type="email" />
+            <input placeholder="Password" type="password"/>
+            <button type="submit"> Sign in</button>
+            
+        </form>
+
+    </div>
+  )
+}
+
+export default SignInScreen
