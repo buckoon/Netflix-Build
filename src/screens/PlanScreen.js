082fb5c42b512c134ce231f180905f2af1c93d06
buckoon <jeremy8@vt.edu>
2023-01-23 07:38:48 -0500

    k

--- src/screens/PlanScreen.js
+++ src/screens/PlanScreen.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import db from '../firebase';
 import './PlanScreen.css';
+import {useSelector} from "react-redux";
+import {selectUser} from "../features/userSlice";
+import { loadStripe } from "@stripe/stripe-js";
 
 function PlanScreen() {
   const [products, setProducts] = useState({});
+  const user = useSelector(selectUser);
 
   useEffect(() => {
     db.collection('products')
@@ -30,7 +34,35 @@ function PlanScreen() {
   }, []);
 
   const loadCheckout = async (priceId) => {
-
+    console.log('loadCheckout function called');
+    const docRef = await db.collection("customers")
+      .doc(user.uid)
+      .collection("checkout_sessions")
+      .add({
+      price: priceId,
+      success_url:window.location.origin,
+      cancel_url:window.location.origin,
+  
+      });
+    docRef.onSnapshot(async(snap) =>{ 
+      const {error, sessionId} = snap.data(); 
+  
+      if (error) {
+        //show an error to your customer and
+        //inspect your cloud function logs inthe Firebase console.
+        alert(`An error occured: ${error.message}`);
+      }
+  
+      if (sessionId) {
+        
+        //we have a session, lets redirect to checkout
+        //init stripe
+  
+        const stripe = await loadStripe("pk_test_51MS0t5B2Q2Sv9OKuJGfXyccMh0tDxdhoLx2jTr1XxozWzAo5EUUpmV5n18oEk3otSug0bP0EVd672tWqD1rCNn4I00W5yvovus")
+  
+        stripe.redirectToCheckout({sessionId});
+      }
+    });
   };
 
   return (
@@ -42,7 +74,8 @@ function PlanScreen() {
               <h5>{productData.name}</h5>
               <h6>{productData.description}</h6>
             </div>
-            <button onClick={() => loadCheckout(productData.prices.priceId)}>Subscribe</button>
+            <button onClick={() => {console.log("Button clicked"); loadCheckout(productData.prices.priceId)}}>Subscribe</button>
+
           </div>
         );
       })}
